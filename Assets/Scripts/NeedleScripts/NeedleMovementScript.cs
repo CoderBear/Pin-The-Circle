12f32fa0776fc543a888fc2fba8fc2b2ab6f1a0f
Joe Ortega <joeortega79@gmail.com>
2018-02-19 10:40:51 +1030

    video2

---
+++ Assets/Scripts/NeedleScripts/NeedleMovementScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleMovementScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject needleBody;
+
+	private bool canFireNeedle;
+	private bool touchedTheCircle;
+
+	private float forceY = 70f;
+
+	private Rigidbody2D myBody;
+
+	void Awake() {
+		Initialize ();
+	}
+
+	void Initialize() {
+		needleBody.SetActive (false);
+		myBody = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (canFireNeedle) {
+			myBody.velocity = new Vector2 (0, forceY);
+		}
+	}
+
+	public void FireTheNeedle(){
+		needleBody.SetActive (true);
+		myBody.isKinematic = false;
+		canFireNeedle = true;
+	}
+}
